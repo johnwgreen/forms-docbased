5f4742ebdb2ef86a3ba25f0a89db4d0f619f1e52
Pranay Tippa <ptippa@adobe.com>
2024-05-24 02:15:55 +0530

    FORMS-14491:RestButton validation on wizard

--- test/e2e/x-walk/resetButton.spec.js
+++ test/e2e/x-walk/resetButton.spec.js
@@ -7,15 +7,14 @@ test.describe('resetButton validation test', () => {
   let page;
   test.beforeAll(async ({ browser }) => {
     page = await browser.newPage();
-      await page.goto(`https://${getCurrentBranch()}--aem-boilerplate-forms--adobe-rnd.hlx.live/drafts/tests/x-walk/wizardvalidation`, { waitUntil: 'networkidle' });
+    await page.goto(`https://${getCurrentBranch()}--aem-boilerplate-forms--adobe-rnd.hlx.live/drafts/tests/x-walk/wizardvalidation`, { waitUntil: 'networkidle' });
   });
 
   test('resetButton validation on wizard panels', async () => {
-
     for (let i = 0; i < 4; i += 1) {
       // eslint-disable-next-line no-await-in-loop
       await page.getByText('Button').click();
-      }
+    }
     await page.getByRole('button', { name: 'Reset' }).click();
     const Count = await page.locator(wizardCount).count();
     expect(Count).toEqual(1);
@@ -27,7 +26,7 @@ test.describe('resetButton validation test', () => {
     for (let i = 0; i < count - 1; i += 1) {
       // eslint-disable-next-line no-await-in-loop
       await page.getByRole('button', { name: 'Next' }).click({ force: true });
-     }
+    }
     await page.getByRole('button', { name: 'Reset' }).click();
     await Promise.all(
       [
@@ -37,4 +36,3 @@ test.describe('resetButton validation test', () => {
     );
   });
 });
-
