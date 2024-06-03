f2da9c7593ecf79d2f00359bb5e4d87715158f5f
Ravisanker E <rae@ravisankers-mbp.macromedia.com>
2024-06-03 15:11:34 +0530

    Set functional coverage to 90

--- test/merge-coverage.js
+++ test/merge-coverage.js
@@ -14,4 +14,4 @@ runCommand('mv coverage/coverage-final.json coverage/coverage-final-unit.json');
 runCommand("c8 --reporter=json --lines 50 npx playwright test --project='chromium'", false); // setting e2e coverage to a minimum of 50 %
 runCommand('mv coverage/coverage-final.json coverage/coverage-final-e2e.json');
 runCommand('nyc merge coverage .nyc_output/coverage.json');
-runCommand('nyc report --check-coverage --lines 92 --functions 94 --branches 92');
+runCommand('nyc report --check-coverage --lines 92 --functions 90 --branches 92');
